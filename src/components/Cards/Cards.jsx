5c6c67fec6ff7805e31badf7f2ba2a4435523473
zeredogo <zeredogo@gmail.com>
2022-07-05 12:06:47 -0700

    updating commit

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './cards.module.css';
 
-const Cards = ({data}) => {
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+
+const Cards = (props) => {
+  console.log(props);
   return (
-    <div>Cards</div>
+    <div className='containerCard'>
+      <Grid container spacing={3} justify='center'>
+         <Grid item component={Card}>
+          <CardContent>
+            <Typography color='textSecondary' gutterBottom>Infected</Typography>
+            <Typography varaint='h5'>Real Data</Typography>
+            <Typography color='textSecondary' >Real Date</Typography>
+            <Typography variant='body2' >Number of active cases of COVID-19</Typography>
+          </CardContent>
+         </Grid>
+      </Grid>
+    </div>
   )
 }
 
