8f99d99251b1c1f6ae8c5b7a0daf782d3f7ced4e
zeredogo <zeredogo@gmail.com>
2022-07-05 10:12:35 -0700

    changes made

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import {Cards, Chart, CountryPicker} from './components'
-function App() {
+import React from 'react';
+import {Cards, Chart, CountryPicker} from './components';
+import {fetchData} from './api';
+
+class App extends React.Component {
+
+  state = {
+    data: {},
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({data: fetchedData})
+  }
+
+  render() {
+    const {data} = this.state;
   return (
     <div className="container">
-      <Cards/>
+      <Cards data={data}/>
       <CountryPicker/>
       <Chart/>
     </div>
   );
+}
 }
 
 export default App;
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Cards = () => {
+const Cards = ({data}) => {
   return (
     <div>Cards</div>
   )
