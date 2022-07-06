13da1088768c19fd68998104f58999142552fdbb
zeredogo <zeredogo@gmail.com>
2022-07-06 16:46:13 -0700

    updating commit

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,44 +1,51 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState , useEffect } from 'react'
 import { fetchDailyData } from '../../api';
-import { Line, Bar } from 'react-chartjs-2';
+import { Line , Bar } from 'react-chartjs-2';
 import styles from './chart.module.css';
 
 const Chart = () => {
   const [dailyData, setDailyData] = useState([]);
-
+    
   useEffect(() => {
     const fetchAPI = async () => {
       setDailyData(await fetchDailyData());
     }
-
+   
     fetchAPI();
   });
-
+ 
   const lineChart = (
-   dailyData.length !== 0
+    dailyData.length !== 0
     ? (
-    <Line
-    data={{
-      labels: dailyData.map(({ date }) => date),
-      datasets: [{
-        data: dailyData.map(({ confirmed }) => confirmed),
-        label: 'Infected',
-        borderColor: '#3333ff',
-        fill: true,
-      }, {
-        data: dailyData.map(({ deaths }) => deaths),
-        label: 'Deaths',
-        borderColor: 'red',
-        backgroundColor: 'rgba(255, 0, 0, 0.5)',
-        fill: true,
-      }],
-    }}
-    />) : null
+      <Line
+       data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [{
+          data: dailyData.map(({ confirmed }) => confirmed),
+          label: 'Infected',
+          borderColor: '#3333ff',
+          fill: true,
+        }, {
+          data: dailyData.map(({ deaths }) => deaths),
+          label: 'Deaths',
+          borderColor: 'red',
+          backgroundColor: 'rgba(255, 0, 0, 0.5)',
+          fill: true,
+        }],
+       }}
+      />) :  null
   );
 
   return (
     <div className={styles.container}>
-    {lineChart}
+    lineChart
+    <Bar
+    data={{
+      labels: ['red', 'blue'],
+    }} 
+    height={400}
+    width={600}
+    />
     </div>
   )
 }
