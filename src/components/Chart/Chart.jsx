d30ac6e4bab5b495f4c611706d1701fe9361ebba
zeredogo <zeredogo@gmail.com>
2022-07-07 15:02:03 -0700

    updating commit

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,7 +3,7 @@ import { fetchDailyData } from '../../api';
 import { Line, Bar } from 'react-chartjs-2';
 import styles from './chart.module.css';
 
-const Chart = () => {
+const Chart = ({data: { confirmed, recovered, deaths }, country}) => {
   const [dailyData, setDailyData] = useState([]);
     
   useEffect(() => {
@@ -37,10 +37,33 @@ const Chart = () => {
     
   );
 
+  const barChart = (
+    confirmed
+    ? (
+      <Bar 
+      data={{
+        labels: ['Infected', 'Recovered', 'Deaths'],
+        datasets: [{
+          label: 'People',
+          backgroundColor: ['rgba(0, 0, 255, 0.5)',
+          'rgba(0, 255, 0, 0.5)',
+          'rgba(255, 0, 0, 0.5)',
+        ],
+        data: [confirmed, recovered, deaths]
+        }]
+      }}
+      options={{
+        legend: {display: false }, 
+        title:{ display: true, text:`current state in ${country}`},
+      }}
+      />
+    ) : null
+  )
+
   return (
     <div className={styles.container}>
     lineChart
-   
+   {barChart}
     </div>
   )
 }
