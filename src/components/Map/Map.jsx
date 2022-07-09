3f40fb9150a129cfcd51be783fc10cc0d4c563bf
zeredogo <zeredogo@gmail.com>
2022-07-09 15:29:34 -0700

    updating css

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,12 +4,8 @@ import { Map as LeafletMap, TileLayer } from 'react-leaflet';
 const Map = () => {
   return (
     <div>
-        <LeafletMap>
-            <TileLayer
-            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-            attribution='&copy; <a href="https://osm.org/copyright">OpenStreet</a>'
-            />
-        </LeafletMap>
+        Hello from Map still working on developing a perfect
+        Map for the project.
     </div>
   )
 }
