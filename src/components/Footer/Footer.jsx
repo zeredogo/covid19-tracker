3981fb820399254c0ba87eba00103bd8713d7c8f
zeredogo <zeredogo@gmail.com>
2022-07-10 17:35:00 -0700

    more commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './footer.module.css';
 
 const Footer = () => {
   return (
     <div>
          <div className={styles.network}>
-        <h5>facebook</h5>
-        <h5>GitHub</h5>
-        <h5>Twitter</h5>
-        <h5>LinkIn</h5>
+        <h5><FontAwesomeIcon icon="fa-brands fa-github" /></h5>
+        <h5><FontAwesomeIcon icon="fa-brands fa-twitter" /></h5>
+        <h5><FontAwesomeIcon icon="fa-brands fa-linkedin" /></h5>
       </div>
       <p className="footer"><small>Dogo Empire &copy; 2022</small></p>
     </div>
