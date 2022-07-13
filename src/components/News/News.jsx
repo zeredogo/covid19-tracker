4a21aa162548143d63b618b8faf68b9437ab20aa
zeredogo <zeredogo@gmail.com>
2022-07-13 14:40:59 -0700

    more update

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,8 +1,7 @@
-import React, { useState ,useEffect} from 'react';
-import { Card, CardContent, Typography, Button, CardMedia, CardActionArea, CardActions } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Card, CardContent, Typography, Grid,  CardActionArea, CardActions } from '@material-ui/core';
 import cx from 'classnames';
 import styles from './news.module.css';
-import photo from './image/kryptozonee.jpeg'
 
 const News = () => {
   const [value, setValue] = useState([]);
@@ -14,43 +13,37 @@ const News = () => {
     console.log(result);
     console.log(result.articles);
 
-   // let p = result.articles.map((a) => { return <div>{a.urlToImage}</div> });
-    //console.log(p);
     setValue(result.articles)
   }
 
   ak ();
 
-  //const getNews = () => {
-    // axios.get("https://newsapi.org/v2/everything?q=tesla&apiKey=1ea292b985c5499aac80f70e120f4476").then((response) => {
-      //console.log(response);
-   //  })
-  //}
-
 
   return (
-    <div>
+    <div className={styles.mainContainer}>
+      <h1 className={styles.header}>Covid-19 News</h1>
       {
         value.map((data) => {
           return (
-            <Card className={cx(styles.card, styles.root)}>
-            <CardActionArea>
-              <CardContent>
-               <img src={data.urlToImage} className={styles.media} alt='coronavirus' />
-                 <Typography color='textSecondary' gutterBottom>{data.title}</Typography>
-                 <Typography variant='body2' color='textSecondary' >{data.description}</Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-             <Button size='small' color='primary'>
-               Read More
-             </Button>
-            </CardActions>
-          </Card>
+            <Grid container spacing={3} justify='center'>
+              <Card className={cx(styles.card, styles.root)}>
+              <CardActionArea>
+                <CardContent>
+                 <img src={data.urlToImage} className={styles.media} alt='coronavirus' />
+                   <Typography color='textSecondary' gutterBottom>{data.title}</Typography>
+                   <Typography variant='body2' color='textSecondary' >{data.description}</Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+               <a href={data.url} size='small' color='primary'>
+                 Main News
+               </a>
+              </CardActions>
+            </Card>
+          </Grid>
           );
         })
       }
-      <h4>hello am </h4>
     </div>
          )
 }
