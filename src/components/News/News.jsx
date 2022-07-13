16b51db488010f5cba4b61f275cd288b8f623071
zeredogo <zeredogo@gmail.com>
2022-07-13 13:43:25 -0700

    more update

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,5 +1,4 @@
 import React, { useState ,useEffect} from 'react';
-import axios from 'axios';
 import { Card, CardContent, Typography, Button, CardMedia, CardActionArea, CardActions } from '@material-ui/core';
 import cx from 'classnames';
 import styles from './news.module.css';
@@ -8,32 +7,52 @@ import photo from './image/kryptozonee.jpeg'
 const News = () => {
   const [value, setValue] = useState([]);
 
-  const getNews = () => {
-     axios.get("https://newsapi.org/v2/everything?q=tesla&apiKey=1ea292b985c5499aac80f70e120f4476")
-     .then((response) => {
-      console.log(response);
-     })
+
+  async function ak() {
+    let response = await fetch("https://newsapi.org/v2/everything?q=coronavirus&apiKey=1ea292b985c5499aac80f70e120f4476");
+    let result = await response.json();
+    console.log(result);
+    console.log(result.articles);
+
+   // let p = result.articles.map((a) => { return <div>{a.urlToImage}</div> });
+    //console.log(p);
+    setValue(result.articles)
   }
+
+  ak ();
+
+  //const getNews = () => {
+    // axios.get("https://newsapi.org/v2/everything?q=tesla&apiKey=1ea292b985c5499aac80f70e120f4476").then((response) => {
+      //console.log(response);
+   //  })
+  //}
 
 
   return (
-         <Card className={cx(styles.card, styles.root)}>
-           <CardActionArea>
-             <CardContent>
-              <img src={photo} className={styles.media} alt='coronavirus' />
-                <Typography color='textSecondary' gutterBottom>Infected</Typography>
-                <Typography varaint='h5'></Typography>
-                <Typography color='textSecondary' >{value}</Typography>
-                <Typography variant='body2' color='textSecondary' >Number of active cases of COVID-19</Typography>
-             </CardContent>
-           </CardActionArea>
-           <CardActions>
-            <Button size='small' color='primary'>
-              Read More
-            </Button>
-           </CardActions>
-         </Card>
-  )
+    <div>
+      {
+        value.map((data) => {
+          return (
+            <Card className={cx(styles.card, styles.root)}>
+            <CardActionArea>
+              <CardContent>
+               <img src={data.urlToImage} className={styles.media} alt='coronavirus' />
+                 <Typography color='textSecondary' gutterBottom>{data.title}</Typography>
+                 <Typography variant='body2' color='textSecondary' >{data.description}</Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+             <Button size='small' color='primary'>
+               Read More
+             </Button>
+            </CardActions>
+          </Card>
+          );
+        })
+      }
+      <h4>hello am </h4>
+    </div>
+         )
 }
 
 export default News
