b0d97009a46e556639ddb116009b606a5bc43e9b
zeredogo <zeredogo@gmail.com>
2022-07-16 21:20:19 +0100

    more update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
         <a href='https://twitter.com/dogoempire1' className={styles.link}><FaTwitter/></a>
         <a href='https://www.linkedin.com/in/paul-dogo-b70910211/' className={styles.link}><FaLinkedin/></a>
       </div>
-      <p className={styles.footer}><small>Krypto Zone &copy; 2022 All right reserve</small></p>
+      <p className={styles.footer}><small>Krypto Zone &copy; 2022 All right reserve.</small></p>
     </div>
   )
 }
